e38c58eee740bdb3bb3e60b69705706ffa48dc6a
n1co1312 <96127729+n1co1312@users.noreply.github.com>
2021-12-14 19:37:17 +0100

    login

--- School/School/Form1.Designer.cs
+++ School/School/Form1.Designer.cs
@@ -29,13 +29,66 @@ namespace School
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Login = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Login.AutoSize = true;
+            this.Login.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.Location = new System.Drawing.Point(665, 24);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(251, 90);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(623, 401);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 50);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.textBox2.Location = new System.Drawing.Point(623, 533);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(335, 50);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1720, 860);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Login);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- School/School/Form1.cs
+++ School/School/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,49 @@ namespace School
         public Form1()
         {
             InitializeComponent();
+            string path = "psw.txt";
+            if (!File.Exists(path)) 
+            {
+                textBox2.Visible = true;
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text == textBox2.Text)
+                {
+                    string psw = textBox2.Text;
+                    string path = "psw.txt";
+                    File.WriteAllText(path, psw);
+                    MessageBox.Show("Passwort efolgreich erstellt", "Passwort");
+                    Application.Restart();
+                }
+                else
+                {
+                    MessageBox.Show("Das erste Passwort muss dem zweitem entsprechen", "Passwort");
+                }
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string path = "psw.txt";
+                string pass = textBox1.Text;
+                string psw = File.ReadAllText(path);
+                if (pass == psw)
+                {
+                    Form2 form2 = new Form2();
+                    Form1 form1 = new Form1();
+                    form2.Show();
+                    form1.WindowState = FormWindowState.Minimized;
+                    form2.WindowState = FormWindowState.Maximized;
+                    
+                }
+            }
         }
     }
 }
